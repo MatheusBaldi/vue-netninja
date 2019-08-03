d0e1853860a50ff3c31c449476d79121a1184de2
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-03 14:30:19 -0300

    #34 Custom Directives: Vue.directive('directiveName', { bind(el, binding, vnode){}}). It is possible to create custom directives and capure the argument and the values passed to it.

--- vue-cli-way/src/main.js
+++ vue-cli-way/src/main.js
@@ -1,10 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource'
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
 
-Vue.use(VueResource)
+Vue.use(VueResource);
 
-Vue.config.productionTip = false
+// Custom Directives
+Vue.directive('rainbow', {
+  bind(el, binding, vnode){
+    el.style.color = "#" + Math.random().toString().slice(2,8)  
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode){
+    if (binding.value == 'wide'){
+      el.style.maxWidth = "1200px";
+    } else if (binding.value == 'narrow'){
+      el.style.maxWidth = "560px";
+    }
+    if (binding.arg == 'column'){
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
+
+
+Vue.config.productionTip = false;
 
 
 export const bus = new Vue();
@@ -12,4 +34,4 @@ export const bus = new Vue();
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
