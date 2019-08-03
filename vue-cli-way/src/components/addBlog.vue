e5dd455e370af8d5dd969a1011a3266a8f63f51e
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-03 12:32:33 -0300

    #32 HTTP Requests: vue-resource

--- vue-cli-way/src/components/addBlog.vue
+++ vue-cli-way/src/components/addBlog.vue
@@ -2,7 +2,7 @@
   <div id="add-blog">
 
     <h2>Add a New Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
 
       <label>Blog title:</label>
       <input type="text" v-model.lazy="blog.title" required/>
@@ -30,7 +30,14 @@
         <option v-bind:key="index" v-for="(author, index) in authors">{{ author }}</option>
       </select>
 
+      <button v-on:click.prevent="post">Add Blog</button>
+
     </form>
+
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
+
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog Title: {{ blog.title }}</p>
@@ -61,11 +68,21 @@ export default{
         categories: [],
         author: ''
       },
-      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+      submitted: false
     }
   },
   methods: {
-
+    post: function(){
+      this.$http.post('https://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1
+      }).then(function(data){
+        console.log(data);
+        this.submitted = true;
+      });
+    }
   }
 }
 </script>
