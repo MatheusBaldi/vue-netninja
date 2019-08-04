6b316ebff6c20cc6f6b7cff82903e8138df79e2e
Matheus Baldi <matheushbaldi@gmail.com>
2019-08-04 20:34:45 -0300

    #42 Route Parameters: In the route parameters the way to indicate a parameter is using a colon and the name of the parameter (:param). To capture it in the component there is a  param added by the VueRouter, so in the component one can have 'param: this..params.id', and then concatenate it to the get url string. Then finally, one of the uses for it is to concatenate it to the 'to' parameter in the <router-link> tag with v-bind (e.g.: v-bind:to="'/resource/' + resource.id")

--- vue-cli-way/src/App.vue
+++ vue-cli-way/src/App.vue
@@ -15,12 +15,13 @@ import showBlogs from './components/showBlogs.vue';
 import listBlogs from './components/listBlogs.vue';
 import header from './components/header.vue';
 
+
 export default{
   components: {
     'add-blog': addBlog,
     'show-blogs': showBlogs,
     'list-blogs': listBlogs,
-    'app-header': header
+    'app-header': header,
   },
   data(){
     return {
